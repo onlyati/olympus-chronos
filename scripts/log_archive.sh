6a346c0e84c44c415aff96bee9df3b20f9d78fc4
Attila Molnar <molnar.attila28@gmail.com>
2022-08-29 17:21:31 +0200

    Add script to archive Chronos logs

---
+++ scripts/log_archive.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+cwd="/etc/olympus/chronos/logs"
+
+cd "${cwd}"
+
+if [[ $? -ne 0 ]]
+then
+	echo "Failed to set '${cwd}' as work directory"
+	exit 1
+fi
+
+# Zip every log file in the logs directory
+date +'%Y%m%d%H%M%S' | awk '{print "zip log_dump_" $1 ".zip *"}' | bash
+
+if [[ $? -ne 0 ]]
+then
+	echo "Failed to zip *.lpg files in ${cwd}"
+	exit 1
+fi
+
+# Delete all log files
+ls -ltra | grep -E "\.log$" | awk '{print "rm -f " $9}' | bash
+
+if [[ $? -ne 0 ]]
+then
+	echo "Failed to delete *.log files in ${cwd}"
+	exit 1
+fi
+
+# Keep only the last 5 archived zip file
+ls -ltra | grep -E "\.zip$" | awk '{print $9}' | head -n -5 | awk '{print "rm -f " $1}' | bash
+
+if [[ $? -ne 0 ]]
+then
+	echo "Failed to cleanup log files in ${cwd}"
+	exit 1
+fi
+
+ls -l
+
+exit 0
+
