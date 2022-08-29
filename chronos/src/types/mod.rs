4823b05373e941de254d29cfe22948cc19288f69
Attila Molnar <molnar.attila28@gmail.com>
2022-08-29 21:34:17 +0200

    Daily check for seconds has been changed to UNIX EPOCH time checking in timer

--- chronos/src/types/mod.rs
+++ chronos/src/types/mod.rs
@@ -1,6 +1,7 @@
 use std::fmt;
-
 use std::time::Duration;
+use std::collections::HashMap;
+use chrono::{Weekday, Local, Datelike, Timelike};
 
 /// Enum for type of timer
 #[derive(PartialEq, Clone)]
@@ -58,14 +59,97 @@ pub struct Timer {
 }
 
 impl Timer {
-    pub fn new(name: String, kind: TimerType, interval: Duration, command: Command, next_hit: u64, days: Vec<bool>) -> Timer {
-        return Timer {
+    pub fn new(name: String, kind: TimerType, interval: Duration, command: Command, days: Vec<bool>) -> Timer {
+        let now_from_unix_epoch = match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+            Ok(n) => n,
+            Err(e) => panic!("Could not calculate time from UNIX_EPOC: {:?}", e),
+        };
+
+        let mut timer = Timer {
             name: name,
             kind: kind,
             interval: interval,
             command: command,
-            next_hit: next_hit,
+            next_hit: now_from_unix_epoch.as_secs(),
             days: days,
+        };
+
+        timer.calculate_next_hit();
+        return timer;
+    }
+
+    pub fn calculate_next_hit(&mut self) {
+        let today_weekday = Self::num_of_today();
+        let now = Local::now().num_seconds_from_midnight();
+        let now: u64 = now.into();
+
+        // We are on that day when the timer has to run
+        if self.days[today_weekday] {
+            if self.kind == TimerType::At {        
+                if now < self.interval.as_secs() {
+                    self.next_hit = self.next_hit - now + self.interval.as_secs();
+                    return;
+                }
+            }
+            else {
+                // For Every and OneShot timers just increment the current value
+                self.next_hit += self.interval.as_secs();
+                return;
+            }
         }
+
+        // From here, timer will not run today but on another day
+        // Set next hit for today 24:00
+        self.next_hit = self.next_hit + (86400 - now);
+
+        // Find out which day need to run and calculate the differenec from today 24:00
+        let next_weekday = Self::find_next_true(&self.days, today_weekday).unwrap();
+        
+        let day_diff = if next_weekday > today_weekday {
+            next_weekday - today_weekday - 1
+        }
+        else {
+            7 + (next_weekday - today_weekday - 1)
+        };
+
+        println!("{} {} {} {} {:?}", self.name, today_weekday, next_weekday, day_diff, self.days);
+        
+        let day_diff = day_diff as u64;
+
+        self.next_hit = self.next_hit + day_diff * 86400 + self.interval.as_secs();
+
+    }
+
+    fn find_next_true(elements: &Vec<bool>, cursor: usize) -> Option<usize> {
+        let next_pos = cursor + 1;
+        for i in next_pos..elements.len() {
+            if elements[i] {
+                println!("asd");
+                return Some(i);
+            }
+        }
+    
+        for i in 0..next_pos {
+            if elements[i] {
+                println!("dfg");
+                return Some(i);
+            }
+        }
+    
+        return None;
+    }
+    
+    fn num_of_today() -> usize {
+        let now = Local::now();
+        let mut day_map: HashMap<Weekday, usize> = HashMap::new();
+        day_map.insert(Weekday::Mon, 0);
+        day_map.insert(Weekday::Tue, 1);
+        day_map.insert(Weekday::Wed, 2);
+        day_map.insert(Weekday::Thu, 3);
+        day_map.insert(Weekday::Fri, 4);
+        day_map.insert(Weekday::Sat, 5);
+        day_map.insert(Weekday::Sun, 6);
+    
+        return *day_map.get(&now.weekday()).unwrap();
     }
 }
